7b4d3750e560d1934de3ff95f2744e24716e83f3
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-10-07 18:30:07 -0400

    Added linked_list.rb with pseudo code for classes and methods

---
+++ linked_list.rb
@@ -0,0 +1,45 @@
+# LinkedList
+  
+  # append(value)
+    # adds new node (value) to end of list
+
+  # prepend(value)
+    # adds new node (value) to beginning of list
+
+  # head
+    # first node of list
+
+  # at(index)
+    # returns node at index
+
+  # tail
+    # last node of list
+
+  # size
+    # total number of nodes
+
+  # pop
+    # removes last element from list
+
+  # contains?(value)
+    # true/false if value is in list
+
+  # find(value)
+    # returns index of node containing value or nil if not found
+
+  # to_s
+    # turns objects into strings to be printed/previewed
+    # format: ( value ) -> ( value ) -> ( value ) -> nil
+
+
+
+    
+
+# Node
+
+  # value
+    # default to 'nil'
+    # points to #next_node
+
+  # next_node
+    # default to 'nil'
