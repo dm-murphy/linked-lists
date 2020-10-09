23aa8cd8f81d92f9f1b9aff43a59774cc5559ac5
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-10-09 14:21:30 -0400

    Added find(value) method

--- linked_list.rb
+++ linked_list.rb
@@ -1,3 +1,11 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil)
+    @value = value
+    @next_node = nil
+  end
+end
 
 class LinkedList
   attr_accessor :head, :tail, :size
@@ -74,26 +82,29 @@ class LinkedList
   def contains?(value)
     node = @head
     @size.times do
-      if node.value == value.to_s
-        return true
-      else
-        node = node.next_node
-      end
+      node.value == value.to_s ? (return true) : node = node.next_node
     end
     false
   end
 
-# find(value)
-    # returns index of node containing value or nil if not found
-
-end
-
-class Node
-  attr_accessor :value, :next_node
-
-  def initialize(value = nil)
-    @value = value
-    @next_node = nil
+  def find(value)
+    if contains?(value) == true
+      node = @head
+      counter = 0
+      index = 0
+      loop do
+        if node.value == value.to_s
+          index = counter
+          break
+        else
+          node = node.next_node
+        end
+        counter += 1
+      end
+      index
+    else
+      nil
+    end
   end
 end
 
@@ -106,12 +117,29 @@ list.prepend('4')
 
 puts list.to_s
 
-#list.pop
-#puts list.to_s
+puts
+puts list.size
+puts list.head
+puts list.tail
+puts
+
+puts list.at(1)
+puts
+
+list.pop
+puts list.to_s
+puts
+
+list.append('8')
 
 puts list.contains?(4)
 puts list.contains?(8)
 puts list.contains?(18)
+puts
+
+puts list.find(4)
+puts list.find(19)
+puts list.find(8)
 
 
 
