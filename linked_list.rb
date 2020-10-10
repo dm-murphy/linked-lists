6145a969a9da37c67e1b4515ff3f25efebc4093b
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-10-09 23:20:20 -0400

    Added extra credit #insert_at. Refactored #find and #at methods into multiple methods. Next step refactor #insert_at into multiple methods.

--- linked_list.rb
+++ linked_list.rb
@@ -67,16 +67,20 @@ class LinkedList
     elsif index >= size || index.negative?
       puts 'Not valid index'
     else
-      node = @head
-      list_index = 0
-      loop do
-        break if list_index == index
-
-        list_index += 1
-        node = node.next_node
-      end
-      node
+      find_node(index)
     end
+  end
+
+  def find_node(index)
+    node = @head
+    list_index = 0
+    loop do
+      break if list_index == index
+
+      list_index += 1
+      node = node.next_node
+    end
+    node
   end
 
   def contains?(value)
@@ -88,24 +92,52 @@ class LinkedList
   end
 
   def find(value)
-    if contains?(value) == true
-      node = @head
-      counter = 0
-      index = 0
-      loop do
-        if node.value == value.to_s
-          index = counter
+    contains?(value) ? find_index(value) : nil
+  end
+    
+  def find_index(value)
+    node = @head
+    counter = 0
+    index = 0
+    loop do
+      if node.value == value.to_s
+        index = counter
+        break
+      else
+        node = node.next_node
+      end
+      counter += 1
+    end
+    index
+  end
+
+  def insert_at(value, index)
+    node = @head
+    #puts node.value # for debugging
+    if index.zero?
+      @head = Node.new(value)
+      #puts @head.value # for debugging
+      @head.next_node = node
+      @size += 1
+    elsif index.negative? || index > @size
+      puts 'Not valid index'
+    else
+      counter = 1
+      (@size).times do # or size if given index is more than @size
+        if index == counter
+          temp_node = node.next_node
+          node.next_node = Node.new(value)
+          node.next_node.next_node = temp_node
+          @size += 1
           break
         else
           node = node.next_node
+          counter += 1
         end
-        counter += 1
       end
-      index
-    else
-      nil
     end
-  end
+  end  
+
 end
 
 list = LinkedList.new
@@ -115,31 +147,41 @@ list.append('8')
 
 list.prepend('4')
 
+# puts list.to_s
+
+# puts
+# puts list.size
+# puts list.head
+# puts list.tail
+# puts
+
+# puts list.at(1)
+# puts
+
+# list.pop
+# puts list.to_s
+# puts
+
+# list.append('8')
+
+# puts list.contains?(4)
+# puts list.contains?(8)
+# puts list.contains?(18)
+# puts
+
+# puts list.find(4)
+# puts list.find(19)
+# puts list.find(8)
+
+puts list.to_s
+list.insert_at('17', 2)
 puts list.to_s
 
 puts
-puts list.size
-puts list.head
-puts list.tail
 puts
-
-puts list.at(1)
 puts
-
-list.pop
-puts list.to_s
-puts
-
-list.append('8')
-
-puts list.contains?(4)
-puts list.contains?(8)
-puts list.contains?(18)
-puts
-
-puts list.find(4)
-puts list.find(19)
-puts list.find(8)
+ puts list.at(1)
+ puts
 
 
 
